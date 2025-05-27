ba49b7289f75bfe840b887908c8525cf634caa87
GautamGulati28 <gautamgulati2809@gmail.com>
2025-05-27 10:55:14 +0530

    update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IoMdReturnRight } from "react-icons/io";
+import { IoMdReturnRight } from "react-icons/io5";
 function Button({title = "Start a project"}) {
   return (
     <div className='max-w-40 px-4 py-2 gap-2 bg-zinc-100 rounded-full text-black flex justify-between items-center'>
